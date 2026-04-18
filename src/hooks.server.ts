c7e5e3924c36d30b297fc11332bd7ba5c5f54b86
vippeterhou <vippeterhou@gmail.com>
2026-04-18 16:57:25 +0800

    Fix app shutdown log: handle SIGTERM with timeout fallback

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -2,7 +2,13 @@ import { logger } from '$lib/server/logger';
 
 logger.info('App started');
 
-process.on('SIGTERM', async () => {
-	await logger.info('App shutting down');
-	process.exit(0);
+process.on('SIGTERM', () => {
+	// Give DB write up to 3s, then exit regardless
+	const timeout = setTimeout(() => process.exit(0), 3000);
+	logger.info('App shutting down')
+		.catch(() => {})
+		.finally(() => {
+			clearTimeout(timeout);
+			process.exit(0);
+		});
 });
